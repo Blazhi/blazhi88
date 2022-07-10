d0e6bd3908e73b3ee7df43d6eb647d3a392a0f25
Blazhi <d.blazhik@gmail.com>
2022-07-10 16:53:03 +0300

    red

--- js/menu.js
+++ js/menu.js
@@ -1,15 +1,14 @@
 (() => {
-  const refs = {
-    openMenulBtn: document.querySelector('[data-menu-open]'),
-    closeMenuBtn: document.querySelector('[data-menu-close]'),
-    menu: document.querySelector('[data-menu]'),
-   
-  };
-
-  refs.openMenuBtn.addEventListener('click', toggleMenu);
-  refs.closeMenuBtn.addEventListener('click', toggleMenu);
-
-  function toggleMenu() {
-    refs.menu.classList.toggle('is-hidden');
-  }
-})();
+    const refs = {
+      openMenuBtn: document.querySelector('.menu-btn-open'),
+      closeMenuBtn: document.querySelector('.menu-btn-close'),
+      menu: document.querySelector('.menu-mob'),
+    };
+  
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  
+    function toggleMenu() {
+      refs.menu.classList.toggle('is-hidden');
+    }
+  })();
